fd45193e748e9b19a480c4d4716cbdedaed89444
metalice17 <metalice@gmail.com>
2020-06-24 18:31:55 +0200

    CSS Adjustments

--- toasterClass.js
+++ toasterClass.js
@@ -1,10 +1,11 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-    typeof define === 'function' && define.amd ? define(['exports'], factory) :
-    (global = global || self, factory(global.Notifications = {}));
-}(this, function (exports) { 'use strict';
+        typeof define === 'function' && define.amd ? define(['exports'], factory) :
+            (global = global || self, factory(global.Notifications = {}));
+}(this, function (exports) {
+    'use strict';
 
-class Toaster {
+    class Toaster {
         constructor() {
             this.ToastEvents = [];
 
@@ -12,45 +13,56 @@ class Toaster {
                 try {
                     var id = Math.random().toString(16).slice(2);
                     var div = document.createElement('div');
-                    div.innerHTML = "<table><tr><td class='left' id='" + id + "left'>" + data.left + "</td><td class='content'><h3 id='" + id + "header'>" + data.header + "</h3><p id='" + id + "description'>" + data.description + "</p><p id='" + id + "message'>" + data.message + "</p></td><td class='right' id='" + id + "right' id='right'>" + data.right + "</td></tr></table>";
+                    div.innerHTML = "<table class='toaster'><tr><td class='left' id='" + id + "left'></td><td class='content'><h3 id='" + id + "header'></h3><p id='" + id + "description'></p><p id='" + id + "message'></p></td><td class='right' id='" + id + "right' id='right'></td></tr></table>";
                     div.setAttribute('class', 'slider close');
                     div.setAttribute('id', id);
                     doc.body.appendChild(div);
+                    this.updateSlice(id, doc, data);
                     return id;
                 }
-                catch (error) { }
+                catch (error) { console.log(error); }
             };
 
             this.updateSlice = function (id, document, data) {
                 if (data != null) {
                     try {
-                        if (data.header != undefined)
-                            document.getElementById(id + 'header').innerHTML = data.header;
-                    }
-                    catch (error) { console.log(error); }
-                    try {
-                        if (data.description != undefined)
-                            document.getElementById(id + 'description').innerHTML = data.description;
-                    }
-                    catch (error) { console.log(error); }
-                    try {
-                        if (data.message != undefined)
-                            document.getElementById(id + 'message').innerHTML = data.message;
-                    }
-                    catch (error) { console.log(error); }
-                    try {
-                        if (data.left != undefined)
-                            document.getElementById(id + 'left').innerHTML = data.left;
-                    }
-                    catch (error) { console.log(error); }
-                    try {
-                        if (data.right != undefined)
-                            document.getElementById(id + 'right').innerHTML = data.right;
-                    }
-                    catch (error) { console.log(error); }
-                    try {
-                        if (data.bgcolor != undefined)
-                            document.getElementById(id).style.backgroundColor = data.bgcolor;
+                        var predelay = 0;
+                        if (data != null) {
+                            if (data.predelay != undefined)
+                                predelay = data.predelay;
+                        }
+                        setTimeout(function () {
+                            try {
+                                if (data.header != undefined)
+                                    document.getElementById(id + 'header').innerHTML = data.header;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.description != undefined)
+                                    document.getElementById(id + 'description').innerHTML = data.description;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.message != undefined)
+                                    document.getElementById(id + 'message').innerHTML = data.message;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.left != undefined)
+                                    document.getElementById(id + 'left').innerHTML = data.left;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.right != undefined && data.right != null)
+                                    document.getElementById(id + 'right').innerHTML = data.right;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.bgcolor != undefined)
+                                    document.getElementById(id).style.backgroundColor = data.bgcolor;
+                            }
+                            catch (error) { console.log(error); }
+                        }, predelay);
                     }
                     catch (error) { console.log(error); }
                 };
@@ -64,58 +76,67 @@ class Toaster {
                         $('#' + id).toggleClass('close');
                     }, 20);
                 }
-                catch (error) { }
+                catch (error) { console.log(error); }
                 return id;
             };
             this.removeSlice = function (id, document, data) {
                 if (data != null) {
                     try {
-                        if (data.header != undefined)
-                            document.getElementById(id + 'header').innerHTML = data.header;
-                    }
-                    catch (error) { }
-                    try {
-                        if (data.description != undefined)
-                            document.getElementById(id + 'description').innerHTML = data.description;
-                    }
-                    catch (error) { }
-                    try {
-                        if (data.message != undefined)
-                            document.getElementById(id + 'message').innerHTML = data.message;
-                    }
-                    catch (error) { }
-                    try {
-                        if (data.left != undefined)
-                            document.getElementById(id + 'left').innerHTML = data.left;
-                    }
-                    catch (error) { console.log(error); }
-                    try {
-                        if (data.right != undefined)
-                            document.getElementById(id + 'right').innerHTML = data.right;
-                    }
-                    catch (error) { }
+                        var predelay = 0;
+                        if (data != null) {
+                            if (data.predelay != undefined)
+                                predelay = data.predelay;
+                        }
+                        setTimeout(function () {
+                            try {
+                                if (data.header != undefined)
+                                    document.getElementById(id + 'header').innerHTML = data.header;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.description != undefined)
+                                    document.getElementById(id + 'description').innerHTML = data.description;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.message != undefined)
+                                    document.getElementById(id + 'message').innerHTML = data.message;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.left != undefined)
+                                    document.getElementById(id + 'left').innerHTML = data.left;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                if (data.right != undefined)
+                                    document.getElementById(id + 'right').innerHTML = data.right;
+                            }
+                            catch (error) { console.log(error); }
+                            try {
+                                $('#' + id).toggleClass('complete');
+                                var delay = 2000;
+                                if (data != null) {
+                                    if (data.delay != undefined)
+                                        delay = data.delay;
+                                }
+                                setTimeout(function () {
+                                    $('#' + id).toggleClass('close');
+                                }, delay);
+                                setTimeout(function () {
+                                    document.body.removeChild(document.getElementById(id));
+                                }, delay + 3000);
+                            }
+                            catch (error) { console.log(error); }
+                        }, predelay);
+                        this.ToastEvents.pop(this.ToastEvents[id]);
+                    } catch (error) { console.log(error); }
                 };
-                try {
-                    this.ToastEvents.pop(this.ToastEvents[id]);
-                    $('#' + id).toggleClass('complete');
-                    var delay = 2000;
-                    if (data != null){
-                        if (data.delay != undefined)
-                            delay = data.delay;
-                    }
-                    setTimeout(function () {
-                        $('#' + id).toggleClass('close');
-                    }, delay);
-                    setTimeout(function () {
-                        document.body.removeChild(document.getElementById(id));
-                    }, delay + 3000);
-                }
-                catch (error) { }
             };
 
         }
     }
-;
-exports.Toaster = Toaster;
+    ;
+    exports.Toaster = Toaster;
 
 }));
